2887128801c1f5eb6b478683f92695c85e94a0e4
manojkrish33 <manojkrish369@gmail.com>
2019-05-29 22:08:35 +0530

    Creating header component and integrating with app

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+	render() {
+		return <h1 className="Header">Welcome to Tic-Tac-Toe</h1>;
+	}
+}
+
+export default Header;
