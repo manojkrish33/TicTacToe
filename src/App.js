dafdb80e6366924ad118d586f0f2f2dbd60eed05
manojkrish33 <manojkrish369@gmail.com>
2019-05-29 22:22:25 +0530

    Create Game component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.css";
-import Header from "./Components/Header/Header";
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+import Game from './Components/Game/Game';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Game />
     </div>
   );
 }
---
+++ src/Components/Game/Game.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+
+class Game extends Component {
+
+    render(){
+        return(
+            <div>
+                <h4>Staus </h4>
+                <h4>Board </h4>
+            </div>
+        );
+    }
+
+}
+
+export default Game;
